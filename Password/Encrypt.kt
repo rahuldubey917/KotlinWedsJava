b01b4c5ec3fa8fd70aa132e412dcbfe2363c4b44
MrAyush <39154110+MrAyush@users.noreply.github.com>
2018-06-13 18:09:19 +0530

    check sum added

--- Password/Encrypt.kt
+++ Password/Encrypt.kt
@@ -3,20 +3,49 @@ package Password
 import java.math.BigInteger
 import java.security.MessageDigest
 
-class Encrypt(str: String) {
-    private val mString: String? = str
+class Encrypt(password: String) {
+
+    private val mString: String? = password
     private var mPassword: String? = null
+    private var mHash: String? = null
+
     fun getString(): String = mString!!
-    fun getEncryptedString(): String {
+
+    fun getEncryptedString(): String? {
         val messageDigest = MessageDigest.getInstance("MD5")
         messageDigest.reset()
         messageDigest.update(mString?.toByteArray(Charsets.UTF_8))
         val digest = messageDigest.digest()
-        val bigInteger = BigInteger(1,digest)
+        val bigInteger = BigInteger(1, digest)
         mPassword = bigInteger.toString(16)
-        while (mPassword?.length!! < 32){
+        while (mPassword?.length!! < 32) {
             mPassword = "0$mPassword"
         }
         return mPassword!!
+    }
+
+    fun generateHash(md5Encrypt: String): String? {
+        mHash = ""
+        for (t in md5Encrypt) {
+            mHash += (0xf - t.toString().toInt(16)).toString(16)
+        }
+        return mHash!!
+    }
+
+    fun checkSum(md5Encrypt: String, hash: String): Boolean{
+        var t = ""
+        var i = 0
+        if (md5Encrypt.length != hash.length){
+            return false
+        }
+        for (md5 in md5Encrypt){
+            t += ((md5.toString().toInt(16)).plus(hash[i++].toString().toInt(16))).toString(16)
+        }
+        for (k in t){
+            if (k != 'f'){
+                return false
+            }
+        }
+        return true
     }
 }
